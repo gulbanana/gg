e47eb86e860013866aab70a9f467dba5468d50d7
Thomas Castiglione <castiglione@mac.com>
2024-03-23 07:10:32 +0800

    restructure unsafe blocks in windows.rs

--- src-tauri/src/windows.rs
+++ src-tauri/src/windows.rs
@@ -14,40 +14,63 @@ use windows::Win32::UI::Shell::{
 };
 
 pub fn reattach_console() {
+    // safety: FFI
     let _ = unsafe { AttachConsole(ATTACH_PARENT_PROCESS) };
 }
 
 #[allow(dead_code)]
 pub fn update_jump_list(recent: &mut Vec<String>, path: &String) -> Result<()> {
+    // create a jump list
+    // safety: FFI
+    let jump_list: ICustomDestinationList =
+        unsafe { CoCreateInstance(&DestinationList, None, CLSCTX_INPROC_SERVER)? };
+
+    // initialise the list and honour removals requested by the user
+    let mut max_destinations = 0u32;
+    let mut destination_path = vec![0u16; MAX_PATH as usize];
+
+    // safety: GetArguments() calls len() on the provided slice, and produces a null-terminated string for PWSTR::to_string()
     unsafe {
-        // init
-        let mut min_slots = 0u32;
-        let jump_list: ICustomDestinationList =
-            CoCreateInstance(&DestinationList, None, CLSCTX_INPROC_SERVER)?;
-        let removed_destinations: IObjectArray = jump_list.BeginList(&mut min_slots)?;
-
-        // check for removals by the user
-        let mut destination_args = vec![0u16; MAX_PATH as usize];
+        let removed_destinations: IObjectArray = jump_list.BeginList(&mut max_destinations)?;
         for i in 0..removed_destinations.GetCount()? {
-            let removed_link: IShellLinkW = removed_destinations.GetAt(i)?;
-            removed_link.GetArguments(&mut destination_args)?;
-            let removed_path = PWSTR::from_raw(destination_args.as_mut_ptr()).to_string()?;
-            recent.retain(|x| *x != removed_path);
+            let removed_link: IShellLinkW = removed_destinations.GetAt(i)?; // safety: i <= GetCount()
+            removed_link.GetArguments(&mut destination_path)?;
+            let removed_path_wstr = PWSTR::from_raw(destination_path.as_mut_ptr());
+            if !removed_path_wstr.is_null() {
+                let removed_path = removed_path_wstr.to_string()?;
+                recent.retain(|x| *x != removed_path);
+            }
         }
+    };
 
-        // add the new path as most-recent and trim to the configured max size
-        recent.retain(|x| x != path);
-        recent.insert(0, path.to_owned());
+    // add the new path as most-recent and trim to the configured max size
+    recent.retain(|x| x != path);
+    recent.insert(0, path.to_owned());
 
-        // turn the paths into IShellLinks
-        let items: IObjectCollection =
-            CoCreateInstance(&EnumerableObjectCollection, None, CLSCTX_INPROC_SERVER)?;
-        for path in recent {
-            let link = create_directory_link(path)?;
+    // safety: FFI
+    let items: IObjectCollection =
+        unsafe { CoCreateInstance(&EnumerableObjectCollection, None, CLSCTX_INPROC_SERVER)? };
+
+    // turn the paths into IShellLinks
+    for path in recent {
+        let path_wstr: HSTRING = (&*path).into();
+        let exe_wstr: HSTRING = std::env::current_exe()?.as_os_str().into();
+        let dir_wstr: HSTRING = Path::new(path)
+            .file_name()
+            .ok_or(anyhow!("repo path is not a directory"))?
+            .into();
+        let dir_wstr = BSTR::from_wide(dir_wstr.as_wide())?;
+
+        // safety: FFI
+        unsafe {
+            let link = create_directory_link(exe_wstr, path_wstr, dir_wstr)?;
             items.AddObject(&link)?;
         }
+    }
 
-        // create a single-category jump list
+    // add a custom category
+    // safety: FFI
+    unsafe {
         let array: IObjectArray = items.cast()?;
         jump_list.AppendCategory(w!("Recent"), &array)?;
         jump_list.CommitList()?;
@@ -56,22 +79,16 @@ pub fn update_jump_list(recent: &mut Vec<String>, path: &String) -> Result<()> {
     Ok(())
 }
 
-unsafe fn create_directory_link(path: &String) -> Result<IShellLinkW> {
-    let exe_wstr: HSTRING = std::env::current_exe()?.as_os_str().into();
-    let path_wstr: HSTRING = path.into();
-    let dir_wstr: HSTRING = Path::new(path)
-        .file_name()
-        .ok_or(anyhow!("repo path is not a directory"))?
-        .into();
-
+// safety: no invariants, it's all FFI
+unsafe fn create_directory_link(path: HSTRING, args: HSTRING, title: BSTR) -> Result<IShellLinkW> {
     let link: IShellLinkW = CoCreateInstance(&ShellLink, None, CLSCTX_INPROC_SERVER)?;
-    link.SetPath(&exe_wstr)?; // launch ourselves...
+    link.SetPath(&path)?; // launch ourselves...
     link.SetIconLocation(w!("%SystemRoot%\\System32\\shell32.dll"), 3)?; // ...with the icon for a directory...
-    link.SetArguments(&path_wstr)?; // ...the directory as an argument...
-    link.SetDescription(&path_wstr)?; // ...and a tooltip containing just the directory name
+    link.SetArguments(&args)?; // ...the directory as an argument...
+    link.SetDescription(&args)?; // ...and a tooltip containing just the directory name
 
     // the actual display string must be set as a property because IShellLink is primarily for shortcuts
-    let title_value = PROPVARIANT::from(BSTR::from_wide(dir_wstr.as_wide())?);
+    let title_value = PROPVARIANT::from(title);
     let mut title_key = PROPERTYKEY::default();
     PSGetPropertyKeyFromName(w!("System.Title"), &mut title_key)?;
 
