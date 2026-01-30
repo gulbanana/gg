5f07fb743aac02c43f02891c1abf5879e2729034
Thomas Castiglione <castiglione@mac.com>
2026-01-31 01:11:32 +0800

    customisable <select> to enable separators in SelectWidget

--- app/controls/SelectWidget.svelte
+++ app/controls/SelectWidget.svelte
@@ -1,4 +1,4 @@
-<script lang="ts" generics="T extends {value: string}">
+<script lang="ts" generics="T extends {value: string, separator?: boolean}">
     import { createEventDispatcher } from "svelte";
     import Icon from "./Icon.svelte";
 
@@ -17,21 +17,32 @@
     let dispatch = createEventDispatcher();
 </script>
 
-<div class="wrapper">
-    <select {id} bind:value on:change={(event) => dispatch("change", event)}>
-        {#each options as option}
+<select {id} bind:value on:change={(event) => dispatch("change", event)}>
+    {#each options as option}
+        {#if option.separator}
+            <hr class="separator" />
+        {:else}
             <option selected={value == option.value} value={option.value}>
                 <slot {option}>{option.value}</slot>
             </option>
-        {/each}
-    </select>
-    <Icon name="chevron-down" />
-</div>
+        {/if}
+    {/each}
+</select>
 
 <style>
+    select,
+    ::picker(select) {
+        appearance: base-select;
+    }
+
     select {
-        appearance: none;
+        height: 30px;
+        min-width: 150px;
         padding-left: 3px;
+        padding-right: 3px;
+
+        display: flex;
+        align-items: center;
 
         cursor: pointer;
 
@@ -40,15 +51,34 @@
         }
     }
 
-    .wrapper {
-        display: flex;
-        position: relative;
+    ::picker(select) {
+        margin-top: 3px;
+        padding-bottom: 6px;
     }
 
-    .wrapper > :global(:last-child) {
-        position: absolute;
-        right: 0;
-        height: 32px;
-        right: 3px;
+    select::picker-icon {
+        content: "";
+        display: block;
+        width: 6px;
+        height: 6px;
+        border-right: 2px solid currentColor;
+        border-bottom: 2px solid currentColor;
+        transform: rotate(45deg);
+        margin-top: -2px;
+        margin-right: 4px;
+    }
+
+    select:open::picker-icon {
+        transform: rotate(225deg);
+        margin-top: 4px;
+    }
+
+    option {
+        outline: none;
+    }
+
+    .separator {
+        border-color: var(--ctp-overlay0);
+        margin: 6px 0;
     }
 </style>
