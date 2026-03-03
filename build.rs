34cb50e38a8c44c6fac4e37987b5b2a06cf3776b
Thomas Castiglione <castiglione@mac.com>
2026-03-03 14:57:12 +0800

    allow (hopefully) a readonly build on docs.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 use tauri_build::Attributes;
 
 fn main() {
+    // docs.rs mounts the source tree read-only; tauri_build writes to gen/schemas/ which would panic.
+    if std::env::var_os("DOCS_RS").is_some() {
+        println!("cargo:rustc-cfg=docsrs");
+        return;
+    }
+
     tauri_build::try_build(Attributes::new().capabilities_path_pattern("gen")).unwrap()
 }
